7fa0b54d6d274d5da210fe4f6e34655eb2714bd2
zBloOnK <mctyano@gmail.com>
2022-07-19 01:54:22 +0200

    Fivem Logo

---
+++ __resource.lua
@@ -0,0 +1,15 @@
+description 'Logo'
+author 'zBloOnKK#6452'
+version 'adamant'
+game 'gta v'
+
+client_scripts {
+	'client.lua'
+}
+
+ui_page('html/index.html')
+
+files({
+  'html/index.html',
+  'html/logo.png'
+})
